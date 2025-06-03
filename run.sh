e0da145d7f6e97afc6b2724487af2a4983954f62
aditya <adityavittal92@gmail.com>
2025-06-03 11:56:51 +0530

    instance create and update

---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+PASSWORD="DevOps321"
+USER="ec2-user"
+DOMAIN="doubtfree.online"
+
+services=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
+
+for service in "${services[@]}"; do
+  echo "Connecting to $service"
+
+  sshpass -p "$PASSWORD" ssh -o StrictHostKeyChecking=no "$USER@$service.$DOMAIN" 'bash -s' <<EOF
+  cd /home/ec2-user
+  sudo dnf install ansible -y
+  if [ ! -d "devops-ansible-roboshop" ]; then
+    git clone https://github.com/vadalibhavya/devops-ansible-roboshop.git
+  fi
+  cd devops-ansible-roboshop
+  ansible-playbook -i inventory.ini -e ansible_user=ec2-user -e ansible_password=DevOps321 $service.yaml
+EOF
+
+done
